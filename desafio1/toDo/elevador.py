3959fde06e224860b0f2cd66c675dd1a6a30d32c
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-06-21 10:46:19 -0300

    refatorando o programa

--- desafio1/toDo/elevador.py
+++ desafio1/toDo/elevador.py
@@ -1,11 +1,21 @@
-from typing import Type, List
+from typing import Type
 
 class Elevador:
 
     # metodos
-    def mudar_andar(self, novo_andar: Type[int], andares: Type[List]) -> Type[list]:
-        posicao = andares.index("atual")
-        andares[posicao] = posicao + 1
-        andares[novo_andar - 1] = "atual"
-        return andares
+    def mudar_andar(self, novo_andar: Type[int]) -> Type[int]:
+        if self.__verificar_andar(novo_andar):
+            return novo_andar
     
+    def mostrar_andar_atual(self, andar: Type[int]) -> Type[str]:
+        if andar == 1:
+            return f"O seu andar atual é o terreo"
+        else:
+            return f"O seu andar atual é {andar}"
+    
+    def __verificar_andar(self, novo_andar: Type[int]) -> Type[bool]:
+        if novo_andar >= 1 and novo_andar <= 15:
+            return True
+        else: 
+            return False
+        
--- desafio1/toDo/predio.py
+++ desafio1/toDo/predio.py
@@ -5,19 +5,12 @@ class Predio:
 
     # contrutor 
     def __init__(self) -> None:
-        self.__andares = ["atual", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] 
+        self.__andar = 1 
         self.__elevador = Elevador()
 
-    # metodos 
-    def mostrar_andar_atual(self) -> Type[str]:
-        return f"O seu andar atual é {self.__andares.index("atual") + 1}"
-    
-    def mudar_andar(self, novo_andar: int) -> Type[None]:
-        if self.__verificar_andar(novo_andar):
-            self.__andares = self.__elevador.mudar_andar(novo_andar, self.__andares)
+    # metodos
+    def mudar_andar(self, novo_andar: Type[int]) -> Type[None]:
+        self.__andar = self.__elevador.mudar_andar(novo_andar)
 
-    def __verificar_andar(self, novo_andar) -> Type[bool]:
-        if novo_andar >= 1 and novo_andar <= 15:
-            return True
-        else: 
-            return False
+    def mostrar_andar_atual(self) -> Type[str]:
+        return self.__elevador.mostrar_andar_atual(self.__andar)
