6ba9a64965e829b832ddf90879086423a666b563
LimaFrancisco <francisco1111.fl@protonmail.com>
2024-07-08 13:03:45 -0300

    testando com outros valores

--- desafio7/toDo/run.py
+++ desafio7/toDo/run.py
@@ -2,5 +2,8 @@ from element import Calculadora
 
 calculadora = Calculadora()
 
-resultado = calculadora.calcular(4, 8)
-print(resultado)
+resultado = calculadora.calcular(2,1)
+print(f'resultado para 2 e 1: {resultado:.2f}')
+
+resultado = calculadora.calcular(4,5)
+print(f'resultado para 4 e 5: {resultado:.2f}')
